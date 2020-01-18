eb57f6a9b06721099c070a1edec796dfc19796d4
Whasnat <49228689+Whasnat@users.noreply.github.com>
2020-01-18 23:37:00 +0600

    Add files via upload

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import getpass
 import time
+
+# Twitter class
 
 
 class TwitterBot:
@@ -9,37 +12,58 @@ class TwitterBot:
         self.password = password
         self.bot = webdriver.Firefox()
 
+    # LOGIN
     def login(self):
         bot = self.bot
-        bot.get('https://twitter.com')
+        bot.get('https://twitter.com/login')
         time.sleep(3)
-        email = bot.find_element_by_class_name('email-input')
-        password = bot.find_element_by_name('session[password]')
+        email = bot.find_element_by_class_name('js-username-field')
+        password = bot.find_element_by_class_name('js-password-field')
         email.clear()
         password.clear()
         email.send_keys(self.username)
+        time.sleep(3)
         password.send_keys(self.password)
-        bot.find_element_by_class_name("EdgeButton").click()
+        time.sleep(3)
+        bot.find_element_by_xpath(
+            '/html/body/div[1]/div[2]/div/div/div[1]/form/div[2]/button').click()
 
+    # LIKE TWEET FUNCTION
     def like_tweet(self, hashtag):
         bot = self.bot
+        # search HASHTAG
         bot.get("https://twitter.com/search?q="+hashtag+"&src=typd")
         time.sleep(3)
-        for i in range(1, 5):
+        for i in range(1, 21):
+            # SCROLL
             bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            time.sleep(3)
-            twts = bot.find_elements_by_class_name('tweet')
-            links = [elem.get_attribute('data-permalink-path')for elem in twts]
+            time.sleep(5)
+            print('\n\n##### IN LIKE_TWEET() #####\n\n')
+
+            # GET_TWEETS
+            twts = bot.find_elements_by_class_name('css-1dbjc4n.r-1loqt21.r-1udh08x')
+            print("TWTS")
+            print(twts)
+
+            # GET_SPECIFIC_TWEET_ID
+            links = [elem.get_attribute("element")for elem in twts]
+            print("LINK")
             print(links)
-            for link in links:
-                bot.get("https://twitter.com" + link)
-                try:
-                    bot.find_element_by_class_name("HeartAnimation").click()
-                    time.sleep(20)
-                except Exception as ex:
-                    time.sleep(120)
+
+            # for link in links:
+            #     print(link)
+            #     bot.get("https://twitter.com/" + link)
+            #     try:
+            #         bot.find_element_by_class_name("HeartAnimation").click()
+            #         time.sleep(10)
+            #     except Exception as ex:
+            #         time.sleep(120)
 
 
-ed = TwitterBot('EMAIL', 'PASSWORD')
+# usrname = input("Enter Username:\t")
+# pwd = str(getpass.getpass(prompt='Enter Password:\t'))
+# ed = TwitterBot(usrname, pwd)
+ed = TwitterBot("WHRahat", "LOLcoding14")
+
 ed.login()
-ed.like_tweet("SEARCH_Item")
+ed.like_tweet("Elon Musk")
